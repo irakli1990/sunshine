f795dd4a1cd58548d1cbcdbbad673c9d1a7b4e7c
Irakli <iraklikardawa@gmail.com>
2022-02-16 21:56:48 +0100

    add: comment to main activity

--- app/src/main/java/com/example/happybearthday/MainActivity.kt
+++ app/src/main/java/com/example/happybearthday/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.happybearthday
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+/**
+ * Gigas pirveli gakvetili
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
